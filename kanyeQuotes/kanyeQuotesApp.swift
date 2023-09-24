04932f1279e339da138b1aa14b4795f318614f6d
Nishaykumar <Nishaykumar24@gmail.com>
2023-09-24 23:23:25 +0530

    Initial Commit

---
+++ kanyeQuotes/kanyeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanyeQuotesApp.swift
+//  kanyeQuotes
+//
+//  Created by Nishay Kumar on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct kanyeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
